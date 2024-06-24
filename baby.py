092473369475971029f3cc94ca66210ce9437da3
Botsalo Lindokuhle Molatsile <168271642+lindowaves@users.noreply.github.com>
2024-06-24 15:35:14 +0200

    Defensive Techniques

--- baby.py
+++ baby.py
@@ -1,13 +1,22 @@
-# This program tests if a user is 18 or older and allowed entry into a party.
+# 1. Implement Defensive Techniques:
+# We need to ensure that the user inputs a 4_digit number as their 'Year Of Birth' by using a While Loop.
+# the len() function enures that the input is 4 characters
+# the isdigit() method ensures that the input is a digit string, not letter string.
+# the int() function converts the string into an integer because we can't subtract integers with a string.
 
-# 1. Ask the user their year of birth
-year = int(input("Enter Year Of Birth: "))
+while True:
+    year = input("Enter Year Of Birth: ")
+    if len(year) == 4 and year.isdigit():
+        year = int(year)
+        break
+    else:
+        print("Invalid Input, please enter a 4-digit number")
 
 # 2. Determine the user's age
 age = (2024 - year)
 
 # 3. Verify if the user meets the age requirment and Display the result.
 if age == 18 or age >= 18:
-    print("Congrats, You Are Old Enough")
-elif age <= 18:
-    print("You Are Not Old Enough")
+    print("Congrats, You Are Old Enough!")
+else:
+    print("You Are Not Old Enough!")
